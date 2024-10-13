652e08c311ea279a58de0b9f62c0060e39ae465d
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-10-13 21:23:19 +0500

    two select

--- src/lib/Comp3.svelte
+++ src/lib/Comp3.svelte
@@ -1,34 +1,54 @@
 <script>
-    export let app_function
+    export let app_function;
+    let coll = [
+        { num: 1, arr: ["a", "b"] },
+        { num: 2, arr: ["c", "d"] },
+        { num: 3, arr: ["e", "f"] },
+    ];
+    let selected_1;
+    let selected_2;
+    // console.log(coll[selected_1].arr)
 </script>
 
-<div class='wrapper'>
-    <div class="card">
-        <img src="vite.svg" alt="Image Description">
-        <h3>Title of the Card</h3>
-        <p>Description of the Card</p>
-        <button type="button">Read More</button>
+<div class="wrapper">
+    <div>
+        <select bind:value={selected_1}>
+            <option  selected disabled>Выберите</option>
+            {#each coll as item, i}
+                <option value={i}>
+                    {item.num}
+                </option>
+            {/each}
+        </select>
+
+        <p>{selected_1}</p>
+    </div>
+
+    <div>
+        <select bind:value={selected_2}>
+            <option  selected disabled>Выберите</option>
+            {#if selected_1 >= 0}
+                {#each coll[selected_1].arr as item, i}
+                    <option value={i}>
+                        {item}
+                    </option>
+                {/each}
+            {/if}
+        </select>
+
+        <p>{selected_2}</p>
     </div>
 </div>
 
 <style>
 
-
-
-
-
-
-
-
-    button{
-        width: 100px;
-        height:50px;
-        background-color: green;
-        color:azure
-    }
-    .wrapper{
-        border:1px solid silver;
+    .wrapper {
+        display: flex;
+        border: 1px solid silver;
         border-radius: 30px;
-        padding:3px;
+        padding: 20px;
+    }
+    .wrapper >div{
+        margin-left: 20px;
     }
 </style>
